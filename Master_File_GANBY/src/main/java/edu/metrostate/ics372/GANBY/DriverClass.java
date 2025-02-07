305c52287c02eabd4458a65bb8626d700d0131ed
Maanyare28 <Maanyare28@gmail.com>
2025-02-06 21:34:19 -0600

    Added a new test for export the JSON File

--- Master_File_GANBY/src/main/java/edu/metrostate/ics372/GANBY/DriverClass.java
+++ Master_File_GANBY/src/main/java/edu/metrostate/ics372/GANBY/DriverClass.java
@@ -3,9 +3,9 @@ package edu.metrostate.ics372.GANBY;
 import edu.metrostate.ics372.GANBY.Catalogs.DealerCatalog;
 import edu.metrostate.ics372.GANBY.Dealer.*;
 import edu.metrostate.ics372.GANBY.Vehicle.*;
+import edu.metrostate.ics372.GANBY.JSON.JSONFileExporter;
 
 import java.time.LocalDateTime;
-
 
 public class DriverClass {
     public static void main(String[] args) {
@@ -16,7 +16,6 @@ public class DriverClass {
 
         Vehicle vehicle2 = new Vehicle(123, 2340, "SportsCar",
                 "Lambo", "Aviation", 200000, LocalDateTime.now());
-
 
         Vehicle vehicle3 = new Vehicle(123, 4000, "Sedan", "Toyota", "Camry",
                 38000, LocalDateTime.now());
@@ -41,13 +40,11 @@ public class DriverClass {
         System.out.println(DealerCatalog.getInstance().getDealerCollection().
                 get(vehicle1.getDealerId()).findVehicleById(vehicle1.getVehicleId()));
 
-
         //Test Get all vehicles for a given dealer
         System.out.println("Getting all vehicles from dealer of id 123: ");
         for (Integer key : DealerCatalog.getInstance().getDealerCollection().get(123).getVehicles().keySet()) {
             System.out.println(DealerCatalog.getInstance().getDealerCollection().get(123).getVehicles().get(key));
         }
-
 
         //Testing disabling Acquisition for dealer
         System.out.println("\nDealer Id 123 Acquisition status: " + DealerCatalog.getInstance().getDealerCollection().
@@ -62,7 +59,6 @@ public class DriverClass {
         DealerCatalog.getInstance().getDealerCollection().get(123).enableVehicleAcquisition();
         System.out.println("Dealer Id 123 Acquisition status after enabled: " + DealerCatalog.getInstance().getDealerCollection().
                 get(123).getVehicleAcquisitionEnabled());
-
 
         //Testing adding vehicle to a disable dealer
         System.out.println("Disabling Dealer Id 123 again:");
@@ -81,6 +77,11 @@ public class DriverClass {
         for (Integer key : DealerCatalog.getInstance().getDealerCollection().keySet()) {
             System.out.println(DealerCatalog.getInstance().getDealerCollection().get(key));
         }
-    }
 
+        // ** New Test for Exporting JSON File **
+        System.out.println("\nTesting JSON File Export for Dealer Id 123...");
+        JSONFileExporter jsonFileExporter = new JSONFileExporter();
+        jsonFileExporter.exportToFile(123);
+        System.out.println("JSON file has been exported for dealer 123.");
+    }
 }
