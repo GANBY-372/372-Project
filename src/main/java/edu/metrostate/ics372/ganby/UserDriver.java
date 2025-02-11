09122b2a9735df7b69f93a16c866bbcc2cd3d67f
Gil Gaitan IV <gaitan@fastmail.com>
2025-02-10 21:20:36 -0600

    Main menu options set to meet requirements

--- src/main/java/edu/metrostate/ics372/ganby/UserDriver.java
+++ src/main/java/edu/metrostate/ics372/ganby/UserDriver.java
@@ -8,28 +8,28 @@ public class UserDriver {
 
     // Method to print the welcome banner
     public static void printWelcomeBanner() {
-        System.out.println("\n\n****************************************************");
+        System.out.println("\n\n*********************************************************");
         System.out.println("*                                                  *");
-        System.out.println("*  Welcome to the Dealership Inventory Manager!    *");
+        System.out.println("*  Welcome to the GANBY Dealership Inventory Manager!    *");
         System.out.println("*                                                  *");
-        System.out.println("****************************************************");
+        System.out.println("*********************************************************");
     }
 
     // Method to print the main menu
     public static void printMainMenu() {
-
-
         System.out.println("\nMain Menu");
-
         System.out.println("----------------------------------------------------");
         System.out.println("Enter the number of the option you would like to select:");
-        System.out.println("\t1. Add a vehicle");
-        System.out.println("\t2. Add a dealer");
-        System.out.println("\t3. List all vehicles");
-        System.out.println("\t4. List all dealers");
-        System.out.println("\t5. Export to JSON");
-        System.out.println("\t6. Import from JSON");
-        System.out.println("\t7. Exit");
+        System.out.println("\t1. Read JSON vehicle file. (This will bring up file explorer)");
+        System.out.println("\t2. Import JSON vehicle file. (This will bring up file explorer)");
+        System.out.println("\t3. Add incoming vehicle");
+        System.out.println("\t4. Enable Vehicle Acquisition");
+        System.out.println("\t5. Disable Vehicle Acquisition");
+        System.out.println("\t6. Export all vehicles from a dealer to a JSON file");
+        System.out.println("\t7. View dealer inventory");
+        System.out.println("\t8. Exit");
         System.out.println("----------------------------------------------------\n");
     }
+
+
 }
