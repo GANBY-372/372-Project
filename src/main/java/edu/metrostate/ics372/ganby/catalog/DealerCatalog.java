f51be0889aa7f610cdefbddb526544d1fd2af278
Gil Gaitan IV <gaitan@fastmail.com>
2025-02-10 22:38:40 -0600

    Comments and formatting

--- src/main/java/edu/metrostate/ics372/ganby/catalog/DealerCatalog.java
+++ src/main/java/edu/metrostate/ics372/ganby/catalog/DealerCatalog.java
@@ -1,16 +1,26 @@
+/**
+ * DealerCatalog.java
+ * @author Banji, Gil
+ * This is a singleton class. Maintains a collection of Dealer objects.
+ * Used to manage the collection of dealers in the system during runtime.
+ * Singleton pattern ensures only one instance of the class is created.
+ */
 package edu.metrostate.ics372.ganby.catalog;
 
 import edu.metrostate.ics372.ganby.dealer.Dealers;
 
 public class DealerCatalog {
 
+    // Private instance
     private static DealerCatalog instance;
-    private final Dealers dealers; //Set<Dealer> dealers;
+    private final Dealers dealers; // Set<Dealer> as defined in Dealers class
 
+    // Private constructor
     private DealerCatalog() {
-        this.dealers = new Dealers(); //dealers = new HashSet<>();
+        this.dealers = new Dealers(); //dealers = new HashSet<>() catalog of dealers;
     }
 
+    // Public static method to access instance
     public static DealerCatalog getInstance() {
         if (instance == null) {
             instance = new DealerCatalog();
@@ -18,6 +28,7 @@ public class DealerCatalog {
         return instance;
     }
 
+    // Public method to get dealers
     public Dealers getDealers() {
         return dealers;
     }
