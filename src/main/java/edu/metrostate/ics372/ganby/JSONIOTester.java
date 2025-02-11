a1de84afe06a8a8ec082a59c70dcd5e0eda693b7
Maanyare28 <Maanyare28@gmail.com>
2025-02-11 12:55:46 -0600

    tested exporter and importer

--- src/main/java/edu/metrostate/ics372/ganby/JSONIOTester.java
+++ src/main/java/edu/metrostate/ics372/ganby/JSONIOTester.java
@@ -1,13 +1,5 @@
-/**
- * JSONIOTester.java
- * @author B, G
- * This class is for testing JSON file import and export.
- * Used for testing.
- */
 package edu.metrostate.ics372.ganby;
 
-import edu.metrostate.ics372.ganby.catalog.DealerCatalog;
-import edu.metrostate.ics372.ganby.catalog.VehicleCatalog;
 import edu.metrostate.ics372.ganby.json.JSONFileExporter;
 import edu.metrostate.ics372.ganby.json.JSONFileImporter;
 
@@ -17,19 +9,15 @@ public class JSONIOTester {
 
     public static void main(String[] args) throws FileNotFoundException, IllegalAccessException {
 
+        // Instantiate JSONFileImporter
         JSONFileImporter jsonFileImporter = new JSONFileImporter();
-//
+
         System.out.println("Importing vehicles...");
         jsonFileImporter.printDealerKeys();
-
         jsonFileImporter.processJSON();
-//        System.out.println(VehicleCatalog.getInstance().getVehicles().size() + " vehicles imported into catalog");
-//        System.out.println(DealerCatalog.getInstance().getDealers().size() + " dealers imported into catalog");
-//
-//        System.out.println(jsonFileImporter.getVehicles().size() + " processed vehicles from JSON");
         System.out.println(jsonFileImporter.getDealers().toString());
-//        System.out.println(jsonFileImporter.getDealers().size() + " processed dealers from JSON");
-//
+
+        // Instantiate JSONFileExporter and test the export functionality
         JSONFileExporter jsonFileExporter = new JSONFileExporter();
         jsonFileExporter.exportToFile();
     }
