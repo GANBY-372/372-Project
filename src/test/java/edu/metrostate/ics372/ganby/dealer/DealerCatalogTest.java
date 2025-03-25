12b7954133a893690eee739406e3930b336a65c6
Gil Gaitan IV <gaitan@fastmail.com>
2025-03-25 17:08:32 -0500

    Started files for other unit tests

---
+++ src/test/java/edu/metrostate/ics372/ganby/dealer/DealerCatalogTest.java
@@ -0,0 +1,78 @@
+package edu.metrostate.ics372.ganby.dealer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerCatalogTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getDealerCatalog() {
+    }
+
+    @Test
+    void getDealerWithId() {
+    }
+
+    @Test
+    void addVehicle() {
+    }
+
+    @Test
+    void modifyVehiclePrice() {
+    }
+
+    @Test
+    void printAllVehiclesByPrice() {
+    }
+
+    @Test
+    void printVehiclesByPriceRange() {
+    }
+
+    @Test
+    void printDealersByInventoryRange() {
+    }
+
+    @Test
+    void printAllDealers() {
+    }
+
+    @Test
+    void printAllVehiclesByDealerId() {
+    }
+
+    @Test
+    void printAllVehiclesOfDealer() {
+    }
+
+    @Test
+    void amountOfAllDealers() {
+    }
+
+    @Test
+    void amountOfAllVehicles() {
+    }
+
+    @Test
+    void enableDealerAcquisition() {
+    }
+
+    @Test
+    void disableDealerAcquisition() {
+    }
+}
---
+++ src/test/java/edu/metrostate/ics372/ganby/dealer/DealerTest.java
@@ -0,0 +1,62 @@
+package edu.metrostate.ics372.ganby.dealer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getDealerId() {
+    }
+
+    @Test
+    void getDealerName() {
+    }
+
+    @Test
+    void getIsVehicleAcquisitionEnabled() {
+    }
+
+    @Test
+    void getVehicleCatalog() {
+    }
+
+    @Test
+    void enableVehicleAcquisition() {
+    }
+
+    @Test
+    void disableVehicleAcquisition() {
+    }
+
+    @Test
+    void findVehicleById() {
+    }
+
+    @Test
+    void addVehicle() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
