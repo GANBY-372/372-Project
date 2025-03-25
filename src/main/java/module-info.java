974de78e0b3a0ee72158a4ba954069744031f384
Banji Lawal <banji.k.lawal@gmail.com>
2025-03-25 16:45:56 -0500

    fixed errors in module-info, DealerController, dealer-view.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,26 @@
 module edu.metrostate.ics372.ganby {
-    requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
+    requires javafx.controls;
+    requires javafx.graphics;
     requires json.simple;
 
+    opens edu.metrostate.ics372.ganby.dealer to javafx.fxml;
+
     exports edu.metrostate.ics372.ganby;
-    exports edu.metrostate.ics372.ganby.dealer to javafx.fxml;  // Export the dealer package to javafx.fxml
-
-    opens edu.metrostate.ics372.ganby to javafx.fxml;
-
 }
+
+//module edu.metrostate.ics372.ganby {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//
+//    requires org.controlsfx.controls;
+//    requires com.dlsc.formsfx;
+//    requires org.kordamp.bootstrapfx.core;
+//    requires json.simple;
+//
+//    exports edu.metrostate.ics372.ganby;
+//    exports edu.metrostate.ics372.ganby.dealer to javafx.fxml;  // Export the dealer package to javafx.fxml
+//
+//    opens edu.metrostate.ics372.ganby to javafx.fxml;
+//
+//}
